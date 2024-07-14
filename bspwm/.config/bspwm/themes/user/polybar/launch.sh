354fcf5b0cac3b40fcaadcc0a1c8ede1af6dbebf
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-07-14 21:21:00 +0530

    update

--- bspwm/.config/bspwm/themes/user/polybar/launch.sh
+++ bspwm/.config/bspwm/themes/user/polybar/launch.sh
@@ -2,7 +2,7 @@
 
 ## Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 CARD="$(light -L | grep 'backlight' | head -n1 | cut -d'/' -f3)"
 INTERFACE="$(ip link | awk '/state UP/ {print $2}' | tr -d :)"
 BAT="$(acpi -b)"
@@ -10,38 +10,39 @@ RFILE="$DIR/.module"
 
 # Fix backlight and network modules
 fix_modules() {
-	if [[ -z "$CARD" ]]; then
-		sed -i -e 's/backlight/bna/g' "$DIR"/config.ini
-	elif [[ "$CARD" != *"intel_"* ]]; then
-		sed -i -e 's/backlight/brightness/g' "$DIR"/config.ini
-	fi
+  if [[ -z "$CARD" ]]; then
+    sed -i -e 's/backlight/bna/g' "$DIR"/config.ini
+  elif [[ "$CARD" != *"intel_"* ]]; then
+    sed -i -e 's/backlight/brightness/g' "$DIR"/config.ini
+  fi
 
-	if [[ -z "$BAT" ]]; then
-		sed -i -e 's/battery/btna/g' "$DIR"/config.ini
-	fi
+  if [[ -z "$BAT" ]]; then
+    sed -i -e 's/battery/btna/g' "$DIR"/config.ini
+  fi
 
-	if [[ "$INTERFACE" == e* ]]; then
-		sed -i -e 's/network/ethernet/g' "$DIR"/config.ini
-	fi
+  if [[ "$INTERFACE" == e* ]]; then
+    sed -i -e 's/network/ethernet/g' "$DIR"/config.ini
+  fi
 }
 
 # Launch the bar
 launch_bar() {
-	# Terminate already running bar instances
-	killall -q polybar
+  # Terminate already running bar instances
+  killall -q polybar
 
-	# Wait until the processes have been shut down
-	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+  # Wait until the processes have been shut down
+  while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-	# Launch the bar
-	for mon in $(polybar --list-monitors | cut -d":" -f1); do
-		MONITOR=$mon polybar -q main -c "$DIR"/config.ini &
-	done
+  # Launch the bar
+  for mon in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$mon polybar -q main -c "$DIR"/config.ini &
+  done
+
 }
 
 # Execute functions
 if [[ ! -f "$RFILE" ]]; then
-	fix_modules
-	touch "$RFILE"
-fi	
+  fix_modules
+  touch "$RFILE"
+fi
 launch_bar
