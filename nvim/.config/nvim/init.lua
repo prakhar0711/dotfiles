732f6f765a02ef94f6788f627280cb2d59943816
prakhar <prakhar.pratyush8010@gmail.com>
2025-04-15 21:58:15 +0530

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,9 +2,3 @@ require("user")
 require("config")
 -- Remove tilde from end of buffer and replace with whitespace
 vim.cmd("let &fillchars='eob: '")
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- vim.cmd.colorscheme("gruvbox-material")
--- vim.cmd.colorscheme("solarized-osaka-night")
-
--- ColorMyPencils("rose-pine-moon")
