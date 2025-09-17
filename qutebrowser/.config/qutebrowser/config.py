25d5c0903222be8d3ea9792ca15a8ad184d1a214
prakhar <prakhar.pratyush8010@gmail.com>
2025-09-17 15:00:22 +0530

    update

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -15,67 +15,61 @@ def read_xresources(prefix):
 
 xresources = read_xresources("*")
 
-# c.colors.statusbar.normal.bg = "#00000000"
-# c.colors.statusbar.command.bg = "#00000000"
+c.colors.statusbar.normal.bg = "#00000000"
+c.colors.statusbar.command.bg = "#00000000"
 # c.colors.statusbar.normal.bg = xresources["*.background"]
 # c.colors.statusbar.command.bg = xresources["*.background"]
-# c.colors.statusbar.command.fg = xresources["*.foreground"]
-# c.colors.statusbar.normal.fg = xresources["*color14"]
-# c.colors.statusbar.passthrough.fg = xresources["*color14"]
-# c.colors.statusbar.url.fg = xresources["*color13"]
-# c.colors.statusbar.url.success.https.fg = xresources["*color13"]
-# c.colors.statusbar.url.hover.fg = xresources["*color12"]
+c.colors.statusbar.command.fg = xresources["*.foreground"]
+c.colors.statusbar.normal.fg = xresources["*color14"]
+c.colors.statusbar.passthrough.fg = xresources["*color14"]
+c.colors.statusbar.url.fg = xresources["*color13"]
+c.colors.statusbar.url.success.https.fg = xresources["*color13"]
+c.colors.statusbar.url.hover.fg = xresources["*color12"]
 # c.statusbar.show = "always"
-# c.colors.tabs.bar.bg = "#00000000"
-# c.colors.tabs.even.bg = "#000000" # transparent tabs!!
-# c.colors.tabs.even.fg = xresources["*.foreground"]
-# # c.colors.tabs.even.bg = xresources["*.background"]
-# c.colors.tabs.selected.even.bg = xresources["*.background"]
-# c.colors.tabs.selected.even.fg = xresources["*.foreground"]
-# c.colors.tabs.odd.bg = "#000000"
-# # c.colors.tabs.odd.bg = xresources["*.background"]
-# c.colors.tabs.odd.fg = xresources["*.foreground"]
-# c.colors.tabs.selected.odd.bg = xresources["*.background"]
-# c.colors.tabs.selected.odd.fg = xresources["*.foreground"]
-# c.colors.hints.bg = xresources["*.background"]
-# c.colors.hints.fg = xresources["*.foreground"]
-# c.tabs.show = "multiple"
-#
-# c.colors.completion.item.selected.match.fg = xresources["*color16"]
-# c.colors.completion.match.fg = xresources["*color16"]
-#
-# c.colors.tabs.indicator.start = xresources["*color10"]
-# c.colors.tabs.indicator.stop = xresources["*color8"]
-# c.colors.completion.odd.bg = xresources["*.background"]
-# c.colors.completion.even.bg = xresources["*.background"]
-# c.colors.completion.fg = xresources["*.foreground"]
-# c.colors.completion.category.bg = xresources["*.background"]
-# c.colors.completion.category.fg = xresources["*.foreground"]
-# c.colors.completion.item.selected.bg = xresources["*.background"]
-# c.colors.completion.item.selected.fg = xresources["*.foreground"]
-#
-# c.colors.messages.info.bg = xresources["*.background"]
-# c.colors.messages.info.fg = xresources["*.foreground"]
-# c.colors.messages.error.bg = xresources["*.background"]
-# c.colors.messages.error.fg = xresources["*.foreground"]
-# c.colors.downloads.error.bg = xresources["*.background"]
-# c.colors.downloads.error.fg = xresources["*.foreground"]
-#
-# c.colors.downloads.bar.bg = xresources["*.background"]
-# c.colors.downloads.start.bg = xresources["*color10"]
-# c.colors.downloads.start.fg = xresources["*.foreground"]
-# c.colors.downloads.stop.bg = xresources["*color8"]
-# c.colors.downloads.stop.fg = xresources["*.foreground"]
-#
-# c.colors.tooltip.bg = xresources["*.background"]
-# c.colors.webpage.bg = xresources["*.background"]
-# c.hints.border = xresources["*.foreground"]
-#
-# # c.url.start_pages = ""
-# # c.url.default_page = ""
-#
-# c.tabs.title.format = "{audio}{current_title}"
-# c.fonts.web.size.default = 20
+c.colors.tabs.even.bg = "#00000000" # transparent tabs!!
+c.colors.tabs.odd.bg = "#00000000"
+c.colors.tabs.bar.bg = "#00000000"
+# c.colors.tabs.even.bg = xresources["*.background"]
+# c.colors.tabs.odd.bg = xresources["*.background"]
+c.colors.tabs.even.fg = xresources["*color0"]
+c.colors.tabs.odd.fg = xresources["*color0"]
+c.colors.tabs.selected.even.bg = xresources["*.foreground"]
+c.colors.tabs.selected.odd.bg = xresources["*.foreground"]
+c.colors.tabs.selected.even.fg = xresources["*.background"]
+c.colors.tabs.selected.odd.fg = xresources["*.background"]
+c.colors.hints.bg = xresources["*.background"]
+c.colors.hints.fg = xresources["*.foreground"]
+c.tabs.show = "multiple"
+
+c.colors.tabs.indicator.start = xresources["*color10"]
+c.colors.tabs.indicator.stop = xresources["*color8"]
+c.colors.completion.odd.bg = xresources["*.background"]
+c.colors.completion.even.bg = xresources["*.background"]
+c.colors.completion.fg = xresources["*.foreground"]
+c.colors.completion.category.bg = xresources["*.background"]
+c.colors.completion.category.fg = xresources["*.foreground"]
+c.colors.completion.item.selected.bg = xresources["*.background"]
+c.colors.completion.item.selected.fg = xresources["*.foreground"]
+
+c.colors.messages.info.bg = xresources["*.background"]
+c.colors.messages.info.fg = xresources["*.foreground"]
+c.colors.messages.error.bg = xresources["*.background"]
+c.colors.messages.error.fg = xresources["*.foreground"]
+c.colors.downloads.error.bg = xresources["*.background"]
+c.colors.downloads.error.fg = xresources["*.foreground"]
+
+c.colors.downloads.bar.bg = xresources["*.background"]
+c.colors.downloads.start.bg = xresources["*color10"]
+c.colors.downloads.start.fg = xresources["*.foreground"]
+c.colors.downloads.stop.bg = xresources["*color8"]
+c.colors.downloads.stop.fg = xresources["*.foreground"]
+
+c.colors.tooltip.bg = xresources["*.background"]
+c.colors.webpage.bg = xresources["*.background"]
+c.hints.border = xresources["*.foreground"]
+
+c.tabs.title.format = "{audio}{current_title}"
+c.fonts.web.size.default = 20
 #
 c.url.searchengines = {
 # note - if you use duckduckgo, you can make use of its built in bangs, of which there are many! https://duckduckgo.com/bangs
@@ -113,7 +107,7 @@ config.bind('gm', 'tab-move')
 
 # dark mode setup
 c.colors.webpage.darkmode.enabled = True
-# c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
+c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
 c.colors.webpage.darkmode.policy.images = 'never'
 config.set('colors.webpage.darkmode.enabled', False, 'file://*')
 
@@ -126,7 +120,7 @@ c.tabs.width = '7%'
 
 # fonts
 c.fonts.default_family = ['JetBrainsMono Nerd Font']
-c.fonts.default_size = '13pt'
+c.fonts.default_size = '16pt'
 c.fonts.web.family.fixed = 'JetBrainsMono Nerd Font'
 c.fonts.web.family.sans_serif = 'JetBrainsMono Nerd Font'
 c.fonts.web.family.serif = 'JetBrainsMono Nerd Font'
