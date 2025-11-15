a35e3896dd50bb7fc9bcb8bf13f10f752ee5808b
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2025-11-15 17:45:43 +0530

    update

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -91,3 +91,32 @@ vim.opt.splitright = true
 -- ========================================
 vim.opt.isfname:append("@-@")
 vim.opt.guicursor = ""
+
+-- highlight yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+    pattern = "*",
+    desc = "highlight selection on yank",
+    callback = function()
+        vim.highlight.on_yank({ timeout = 200, visual = true })
+    end,
+})
+
+-- open help in vertical split
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "help",
+    command = "wincmd L",
+})
+
+-- auto resize splits when the terminal's window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+    command = "wincmd =",
+})
+
+-- no auto continue comments on new line
+vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+    callback = function()
+        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+    end,
+})
