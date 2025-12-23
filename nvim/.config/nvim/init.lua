d88575f0824af29853412ee8d165f597462230c3
prakhar <prakhar.pratyush8010@gmail.com>
2025-12-23 13:42:41 +0530

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
-if vim.loader then
-    vim.loader.enable()
-end
+-- if vim.loader then
+--     vim.loader.enable()
+-- end
 
 require("user.options")
 require("user.keymaps")
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,85 +1,88 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		-- lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" }, keys = { { "<c-space>", desc = "Increment Selection" },
-			{ "<bs>", desc = "Decrement Selection", mode = "x" },
-		},
-		opts_extend = { "ensure_installed" },
-		---@type TSConfig
-		---@diagnostic disable-next-line: missing-fields
-		opts = {
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-			ensure_installed = {
-				"cpp",
-				"c",
-				"rust",
-				"html",
-				"javascript",
-				"typescript",
-				"json",
-				"java",
-				"lua",
-				"luadoc",
-				"regex",
-				"tsx",
-				"vim",
-				"vimdoc",
-			},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
-			textobjects = {
-				move = {
-					enable = true,
-					goto_next_start = {
-						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
-						["]a"] = "@parameter.inner",
-					},
-					goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
-						["[a"] = "@parameter.inner",
-					},
-					goto_previous_end = {
-						["[F"] = "@function.outer",
-						["[C"] = "@class.outer",
-						["[A"] = "@parameter.inner",
-					},
-				},
-			},
-		},
-		---@param opts TSConfig
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		config = function()
-			require("treesitter-context").setup({
-				enable = true,
-				max_lines = 0,
-				trim_scope = "inner",
-			})
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        branch = "master",
+        lazy = false,
+        -- lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        keys = { { "<c-space>", desc = "Increment Selection" },
+            { "<bs>",      desc = "Decrement Selection", mode = "x" },
+        },
+        opts_extend = { "ensure_installed" },
+        ---@type TSConfig
+        ---@diagnostic disable-next-line: missing-fields
+        opts = {
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+            ensure_installed = {
+                "cpp",
+                "c",
+                "rust",
+                "html",
+                "javascript",
+                "typescript",
+                "json",
+                "java",
+                "lua",
+                "luadoc",
+                "regex",
+                "tsx",
+                "vim",
+                "vimdoc",
+            },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = {
+                enable = true,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+            textobjects = {
+                move = {
+                    enable = true,
+                    goto_next_start = {
+                        ["]f"] = "@function.outer",
+                        ["]c"] = "@class.outer",
+                        ["]a"] = "@parameter.inner",
+                    },
+                    goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+                    goto_previous_start = {
+                        ["[f"] = "@function.outer",
+                        ["[c"] = "@class.outer",
+                        ["[a"] = "@parameter.inner",
+                    },
+                    goto_previous_end = {
+                        ["[F"] = "@function.outer",
+                        ["[C"] = "@class.outer",
+                        ["[A"] = "@parameter.inner",
+                    },
+                },
+            },
+        },
+        ---@param opts TSConfig
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require("treesitter-context").setup({
+                enable = true,
+                max_lines = 0,
+                trim_scope = "inner",
+            })
+        end,
+    },
 }
