b4e3b582a65b17a8ca7e632ff44fe03cca749e4e
prakhar <prakhar.pratyush8010@gmail.com>
2026-06-30 00:00:12 +0530

    update

--- hypr/.config/hypr/lua/rules.lua
+++ hypr/.config/hypr/lua/rules.lua
@@ -1,19 +1,21 @@
 local workspace_assignments = {
-        [1] = "^(kitty|Alacritty|foot|wezterm)$",
-        [2] = "^([Ff]irefox|[Tt]horium)$",
+    [1] = "^(kitty|Alacritty|foot|wezterm)$",
+    [2] = "^([Ff]irefox|[Tt]horium)$",
+    [3] = "^(org.gnome.Nautilus|org.kde.dolphin)$",
+    [4] = "^([Ss]potify)$"
 }
 
 for ws, regex in pairs(workspace_assignments) do
-        hl.window_rule({
-                name      = "assign-workspace-" .. ws,
-                match     = { class = regex },
-                workspace = ws
-        })
+    hl.window_rule({
+        name      = "assign-workspace-" .. ws,
+        match     = { class = regex },
+        workspace = ws
+    })
 end
 
 -- Core System Layout Behavior Rules
 hl.window_rule({
-        name     = "fix-xwayland-drags",
-        match    = { class = "^$", title = "^$", xwayland = true, float = true },
-        no_focus = true,
+    name     = "fix-xwayland-drags",
+    match    = { class = "^$", title = "^$", xwayland = true, float = true },
+    no_focus = true,
 })
